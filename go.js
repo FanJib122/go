41f11dc3c870d11b05788fc226781ddb891c439e
FanJib122 <30363827+FanJib122@users.noreply.github.com>
2019-10-17 01:08:36 +0700

    Update go.js

--- go.js
+++ go.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "http://www.blogcyber1.com/p/go.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://blogcyber1.blogspot.com/p/go.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
